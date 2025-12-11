0fb1c73734e1594ff1da66ab90802b636410491c
Liekki777 <liekkistore@gmail.com>
2025-12-11 15:59:19 +0100

    fix netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
+import pagefind from 'astro-pagefind';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()]
+  integrations: [tailwind(), preact(), pagefind()],
+  adapter: netlify()
 });
