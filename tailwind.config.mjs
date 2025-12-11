c62bf9d22e8eca7dbabc98c99f0e40a7c58453e7
Liekki777 <liekkistore@gmail.com>
2025-12-11 17:33:10 +0100

    add dark theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class', // Habilitar modo oscuro basado en clase
   theme: {
     extend: {},
   },
