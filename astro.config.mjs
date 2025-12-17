296a3b1c03bd458945667a2fb089167875f62b9f
Liekki777 <liekkistore@gmail.com>
2025-12-17 18:32:48 +0100

    fuck netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import pagefind from 'astro-pagefind';
-import netlify from '@astrojs/netlify';
+// import netlify from '@astrojs/netlify'; <--- ELIMINA O COMENTA ESTO
 
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  // --- CONFIGURACIÓN PARA GITHUB PAGES ---
+  // Reemplaza con tu usuario real de GitHub
+  site: 'https://TU_USUARIO.github.io',
+  // El nombre de tu repositorio
+  base: '/lemoneWeb', 
+  // ---------------------------------------
+
   integrations: [tailwind(), pagefind(), react()],
-  adapter: netlify()
+  
+  // adapter: netlify() <--- ELIMINA O COMENTA ESTO
 });
